83ce1cb5f04fc1b626a0fd717da8f1d1d2735f65
Luka28Lagos <luka28lagos@gmail.com>
2018-05-27 15:32:48 -0300

    Method getUsers created

--- src/main/java/com/usuarios/demo/service/UserService.java
+++ src/main/java/com/usuarios/demo/service/UserService.java
@@ -1,9 +1,22 @@
 package com.usuarios.demo.service;
 
+import java.util.List;
+
 import com.usuarios.demo.model.User;
 
 public interface UserService {
 
+	/**
+	 * Guarda un Usuario
+	 * @param user
+	 * @return Usuario Guardado
+	 */
 	User save(User user);
+
+	/**
+	 * Recupera la Lista de Usuarios
+	 * @return Lista de Usuarios
+	 */
+	List<User> findAll();
 
 }
